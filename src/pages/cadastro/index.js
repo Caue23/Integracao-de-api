a25d0eb30da7cbc0e0eaa1e03a1f1ad35c12207c
CaueSantos <cauevinicius231@gmail.com>
2023-05-12 09:34:29 -0300

    feat: atualizacao

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import "./CadastroForm.css";
 import { useNavigate } from 'react-router';
 import { FaArrowLeft } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-export  function CadastroForm() {
+export function CadastroForm() {
   const [user, setUser] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -28,9 +29,11 @@ export  function CadastroForm() {
       })
       .then((response) => {
         console.log(response);
+        toast.success('Usuário cadastrado com sucesso!');
       })
       .catch((error) => {
         console.log(error);
+        toast.error('Erro ao cadastrar o usuário.');
       });
   };
 
@@ -38,59 +41,60 @@ export  function CadastroForm() {
     <div className="container">
       <a className="backButton" onClick={handleBack}><FaArrowLeft/></a>
       <h1>Cadastro</h1>
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="user" className="form-label">
-          Usuário:
-        </label>
-        <input
-          type="text"
-          id="user"
-          className="form-input"
-          value={user}
-          onChange={(event) => setUser(event.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email" className="form-label">
-          E-mail:
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="form-input"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password" className="form-label">
-          Senha:
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="form-input"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="name" className="form-label">
-          Nome:
-        </label>
-        <input
-          type="text"
-          id="name"
-          className="form-input"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <button type="submit" className="form-button">
-        Enviar
-      </button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="user" className="form-label">
+            Usuário:
+          </label>
+          <input
+            type="text"
+            id="user"
+            className="form-input"
+            value={user}
+            onChange={(event) => setUser(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            E-mail:
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="form-input"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">
+            Senha:
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-input"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name" className="form-label">
+            Nome:
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <button type="submit" className="form-button">
+          Enviar
+        </button>
+      </form>
+      <ToastContainer />
     </div>
   )
 }
